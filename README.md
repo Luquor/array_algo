853f3fc09011c53f90449bc54b6805d88baa4549
Luquor <signorellilucas7@gmail.com>
2025-12-26 11:25:39 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Disclaimer
+/!\ This program is only for learning purposes.
+
+It contains some basic algorithmes based on arrays : min, max, average & some sorting
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let numbers: [i32; 100] = [
+      812, 476, -135, 993, 58, 631, 417, 205, -42, 904,
+      337, 788, 129, 552, 682, 416, -87, 949, 271, 705,
+      564, 873, 492, 118, 305, 640, 721, 867, 430, -196,
+      958, 284, 613, 744, 199, 532, 381, 827, 902, 147,
+      -63, 511, 674, 388, 596, 248, 472, 905, 334, 781,
+      146, 664, -214, 873, 520, 437, 792, 365, 688, -51,
+      998, 289, 577, 806, 254, 612, 349, 934, 150, 478,
+      763, 224, 571, 856, 432, 699, 118, -102, 985, 310,
+      547, 903, 206, 752, 498, 377, 845, 268, 639, 721,
+      401, 874, 133, 566, 918, 247, -188, 664, 292, 807,
+    ];
+
+    println!("---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---    ---");
+    println!("---     This program will do some calculation on the following array     ---");
+    println!("---                    (e.g. min, max, average, sort)                    ---");
+    println!("---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---    ---");
+    println!();
+
+    let lowest = find_min(&numbers);
+    let highest = find_max(&numbers);
+    let average = calculate_average(&numbers);
+
+    println!("Numbers: {:?}", numbers);
+    println!("Lowest value: {}", lowest);
+    println!("Highest value: {}", highest);
+    println!("Average value: {}", average);
+
+}
+
+fn find_min(arr: &[i32]) -> i32 {
+    let mut lowest = arr[0];
+    for &value in arr {
+        if value < lowest {
+            lowest = value;
+        }
+    }
+    return lowest;
+}
+
+fn find_max(arr: &[i32]) -> i32 {
+    let mut highest = arr[0];
+    for &value in arr {
+        if value > highest {
+            highest = value;
+        }
+    }
+    return highest;
+}
+
+fn calculate_average(arr: &[i32]) -> f64 {
+    let mut sum = 0;
+    for &value in arr {
+        sum += value;
+    }
+    return sum as f64 / arr.len() as f64;
+}
